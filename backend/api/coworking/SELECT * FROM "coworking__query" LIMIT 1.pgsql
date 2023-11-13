7ca8c4823cbc3856afecc4e8da5a700c2b1af3ea
robotsouper <qingyuan@ad.unc.edu>
2023-11-12 21:48:21 -0500

    github problem fixed

---
+++ backend/api/coworking/SELECT * FROM "coworking__query" LIMIT 1.pgsql
@@ -0,0 +1,1 @@
+SELECT * FROM "coworking__query" LIMIT 1000;
---
+++ backend/models/coworking/query.py
@@ -0,0 +1,17 @@
+"""Models for statistic query history."""
+
+
+from pydantic import BaseModel
+from .time_range import TimeRange
+from datetime import datetime
+
+
+class Query(BaseModel):
+    """The query history of the XL."""
+
+    id: int | None = None
+    name: str = ""
+    start_date: datetime
+    end_date: datetime
+    compare_start_date: datetime
+    compare_end_date: datetime
