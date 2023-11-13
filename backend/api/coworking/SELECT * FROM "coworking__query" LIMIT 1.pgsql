78a5e22b847c6ab4c4218a424c4c93698c9f05ac
robotsouper <qingyuan@ad.unc.edu>
2023-11-13 10:52:23 -0500

    add backend method for the query table(both the service and the connection) -add methods for get-all, add, and delete in backend/api/coworking/query -intergrate the latest version of the sql table as well I do not know how the id work, but it would be best if we can control the assigned id

--- backend/api/coworking/SELECT * FROM "coworking__query" LIMIT 1.pgsql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM "coworking__query" LIMIT 1000;
--- backend/api/coworking/query.py
+++ backend/api/coworking/query.py
@@ -1,32 +1,42 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
+from backend.api.authentication import registered_user
+from backend.models.user import User
 from ...database import db_session
 from ...services.coworking.query import QueryService
 from ...models.coworking import Query
 
-api = APIRouter(prefix="/api/admin/queries")
+openapi_tags = {
+    "name": "Coworking",
+    "description": "Methods to handle requests of queries",
+}
+
+api = APIRouter(prefix="/api/coworking/queries")
 
 
-@api.get("/get-all-query/", response_model=List[Query], tags=["(Admin) Roles"])
-def get_all_queries(query_svc: QueryService = Depends(QueryService)) -> List[Query]:
+@api.get("/get-all-query/", response_model=List[Query], tags=["Coworking"])
+def get_all_queries(
+    subject: User = Depends(registered_user),
+    query_svc: QueryService = Depends(QueryService),
+) -> List[Query]:
     return query_svc.get_all()
 
 
-@api.post("/create-query", response_model=Query, tags=["(Admin) Roles"])
+@api.post("/create-query", response_model=Query, tags=["Coworking"])
 def create_query(
     query: Query, query_svc: QueryService = Depends(QueryService)
 ) -> Query:
     return query_svc.add(query.model_dump())
 
 
-@api.delete("/delete-query/{query_id}", response_model=Query, tags=["(Admin) Roles"])
+@api.delete("/delete-query/{query_id}", response_model=bool, tags=["Coworking"])
 def delete_query(
     query_id: int, query_svc: QueryService = Depends(QueryService)
-) -> Query:
+) -> bool:
     deleted_query = query_svc.delete(query_id)
     if not deleted_query:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Query not found"
         )
-    return deleted_query  # type: ignore
+    return deleted_query
