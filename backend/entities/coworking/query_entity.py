44f3adc8fc1627f5161c474c91eb8a4bc126f836
robotsouper <qingyuan@ad.unc.edu>
2023-11-15 10:35:00 -0500

    added share feature in the query table in preparation for the last story

--- backend/entities/coworking/query_entity.py
+++ backend/entities/coworking/query_entity.py
@@ -1,6 +1,6 @@
 """Entity for Query.""" ""
 
-from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import Integer, String, DateTime, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from ..entity_base import EntityBase
 from ...models.coworking import Query  # type: ignore
@@ -19,6 +19,7 @@ class QueryEntity(EntityBase):
     end_date: Mapped[datetime] = mapped_column(DateTime, index=True)
     compare_start_date: Mapped[Optional[datetime]] = mapped_column(DateTime, index=True)
     compare_end_date: Mapped[Optional[datetime]] = mapped_column(DateTime, index=True)
+    share: Mapped[bool] = mapped_column(Boolean, default=False)
 
     def to_model(self) -> Query:
         return Query(
@@ -28,6 +29,7 @@ class QueryEntity(EntityBase):
             end_date=self.end_date,
             compare_start_date=self.compare_start_date,
             compare_end_date=self.compare_end_date,
+            share=self.share,
         )
 
     @classmethod
@@ -39,4 +41,5 @@ class QueryEntity(EntityBase):
             end_date=model.end_date,
             compare_start_date=model.compare_start_date,
             compare_end_date=model.compare_end_date,
+            share=model.share,
         )
